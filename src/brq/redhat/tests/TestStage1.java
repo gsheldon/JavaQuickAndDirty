ce7dd31741950985d39ee6cfa1432cb845a4ffda
Marek Czernek <mczernek@redhat.com>
2017-08-23 10:58:26 +0200

    Make Test for Stage1 more readable

--- src/brq/redhat/tests/TestStage1.java
+++ src/brq/redhat/tests/TestStage1.java
@@ -26,13 +26,20 @@ public class TestStage1 {
 
 	@Test
 	public void testPreparingPerson() {
-		assertThat(p).as("The Stage1.preparePerson method returns null when it is not implemented. It should return a Person instance").isNotNull();
-		assertThat(p.getName()).as("The Person.getName() method is unimplemented. It should return set name.").isEqualTo(PERSON_NAME);
+		assertThat(p)
+                .as("The Stage1.preparePerson method returns null when it is not implemented. It should return a Person instance")
+                .isNotNull();
+
+		assertThat(p.getName())
+                .as("The Person.getName() method is unimplemented. It should return set name.")
+                .isEqualTo(PERSON_NAME);
     }
 	
 	@Test
 	public void testPersonGreetings() {
-		assertThat(p.greetings()).as("The greeting String should contain "+PERSON_NAME).contains(PERSON_NAME);
+		assertThat(p.greetings())
+                .as("The greeting String should contain %s",PERSON_NAME)
+                .contains(PERSON_NAME);
 	}
 	
 	@Test
@@ -40,11 +47,16 @@ public class TestStage1 {
 		Accountant a = new Accountant();
 		
 		BigDecimal givenSalary = new BigDecimal(1000);
-		double tax = 0.1;
+		double tax = 0.1; //10%
 		a.setSalary(givenSalary);
 		
-		assertThat(a.countTaxes(givenSalary, tax)).as("Counting taxes out of set, external salary.").isEqualTo(100.0);
-		assertThat(a.countTaxes(null, tax)).as("Counting taxes out of its own salary.").isEqualTo(100.0);
+		assertThat(a.countTaxes(givenSalary, tax))
+                .as("Counting taxes out of set, external salary.")
+                .isEqualTo(100.0);
+
+		assertThat(a.countTaxes(null, tax))
+                .as("Counting taxes out of its own salary.")
+                .isEqualTo(100.0);
 	}
 
 }
